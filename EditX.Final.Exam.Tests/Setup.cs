5dc8b890d1eb307d5ca9145ed6e3f8cba3da5b87
Lance-88 <129681882+Lance-88@users.noreply.github.com>
2025-05-15 15:20:23 +0200

    Invariant Culture for all tests

---
+++ EditX.Final.Exam.Tests/Setup.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace EditX.Final.Exam.Tests;
+
+[TestClass]
+public class AssemblySetup
+{
+    [AssemblyInitialize]
+    public static void AssemblyInit(TestContext context)
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+    }
+}
